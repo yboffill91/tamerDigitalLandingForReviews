31bda7ef856ad6e7f49c5103708ec3e642361d71
Yasmany Boffill <yboffill91@gmail.com>
2025-04-07 14:26:11 -0400

    fix: deleting export on page

--- src/app/(tools)/website-calculator/(steps)/themeStep/page.tsx
+++ src/app/(tools)/website-calculator/(steps)/themeStep/page.tsx
@@ -1,11 +1,7 @@
-import { ThemeStep } from '@/features/tools/websiteCalculator/components/steps/ThemeStep'
+import { ThemeStep } from '@/features/tools/websiteCalculator/components/steps/ThemeStep';
 
-export  const WebCalcHostingPage = ()=> {
-  return (
-    
-      <ThemeStep />
-    
-  )
-}
+const WebCalcHostingPage = () => {
+  return <ThemeStep />;
+};
 
-export default WebCalcHostingPage
+export default WebCalcHostingPage;
