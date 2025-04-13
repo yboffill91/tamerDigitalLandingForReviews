92a57ba3cd17f78d6389593eed73b572955c0a9e
Yasmany Boffill <yboffill91@gmail.com>
2025-04-12 22:10:54 -0400

    Add initial landing page with hero section and background image

--- src/features/landing/LandingPage.tsx
+++ src/features/landing/LandingPage.tsx
@@ -1,74 +1,82 @@
 import {
   HeroSectionWithBeamsAndGrid,
-  // Cards,
-  Locations,
-  FeaturesTabs,
-  SEOResultsCard,
-  Features,
-  Services,
-  SocialMedia,
-  Reviews,
-  FAQ,
-  WhyNeedUs,
-  Process,
-  ReadyToGrow,
-  FreeTools,
+  // Locations,
+  // FeaturesTabs,
+  // SEOResultsCard,
+  // Features,
+  // Services,
+  // SocialMedia,
+  // Reviews,
+  // FAQ,
+  // WhyNeedUs,
+  // Process,
+  // ReadyToGrow,
+  // FreeTools,
 } from '@/features/landing/components';
 
 const LandingPage = () => {
   return (
-    <main className="relative antialiased overflow-x-hidden flex flex-col gap-y-8 ">
+    // <main className="relative antialiased overflow-x-hidden flex flex-col gap-y-8 ">
+    //   <section
+    //     aria-labelledby="hero-title"
+    //     className="relative"
+    //     style={{
+    //       background: 'url(/features/landing/hero_background_att.webp)',
+    //       backgroundSize: 'cover',
+    //       backgroundRepeat: 'no-repeat',
+    //     }}
+    //   >
+    //     <div className="absolute z-0 bottom-0 left-0 h-96 w-screen bg-gradient-to-b from-transparent from-20% to-background-primary" />
+    //     <HeroSectionWithBeamsAndGrid />
+    //   </section>
+    //   <section aria-labelledby="Analitycs" className="relative   ">
+    //     <WhyNeedUs />
+    //   </section>
+    //   <section aria-labelledby="Locations" className="relative   ">
+    //     <Locations />
+    //   </section>
+    //   <section aria-labelledby="features">
+    //     <div className="   px-2">
+    //       <FeaturesTabs />
+    //       <SEOResultsCard />
+    //     </div>
+    //   </section>
+    //   <section aria-labelledby="Features" className="  px-2">
+    //     <Features />
+    //   </section>
+    //   <section aria-labelledby="Services" className="  px-2">
+    //     <Services />
+    //   </section>
+    //   <section>
+    //     <Process />
+    //   </section>
+    //   <section>
+    //     <ReadyToGrow />
+    //   </section>
+
+    //   <section>
+    //     <SocialMedia />
+    //   </section>
+
+    //   <section aria-labelledby="Reviews" className="">
+    //     <Reviews />
+    //   </section>
+    //   <section aria-labelledby="Reviews" className="">
+    //     <FreeTools />
+    //   </section>
+    //   <section aria-labelledby="FAQ  px-2">
+    //     <FAQ />
+    //   </section>
+    // </main>
+    <main>
       <section
-        aria-labelledby="hero-title"
-        className="relative"
         style={{
           background: 'url(/features/landing/hero_background_att.webp)',
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
         }}
       >
-        <div className="absolute z-0 bottom-0 left-0 h-96 w-screen bg-gradient-to-b from-transparent from-20% to-background-primary" />
         <HeroSectionWithBeamsAndGrid />
-      </section>
-      <section aria-labelledby="Analitycs" className="relative   ">
-        {/* <Cards />
-         */}
-        <WhyNeedUs />
-      </section>
-      <section aria-labelledby="Locations" className="relative   ">
-        <Locations />
-      </section>
-      <section aria-labelledby="features">
-        <div className="   px-2">
-          <FeaturesTabs />
-          <SEOResultsCard />
-        </div>
-      </section>
-      <section aria-labelledby="Features" className="  px-2">
-        <Features />
-      </section>
-      <section aria-labelledby="Services" className="  px-2">
-        <Services />
-      </section>
-      <section>
-        <Process />
-      </section>
-      <section>
-        <ReadyToGrow />
-      </section>
-
-      <section>
-        <SocialMedia />
-      </section>
-
-      <section aria-labelledby="Reviews" className="">
-        <Reviews />
-      </section>
-      <section aria-labelledby="Reviews" className="">
-        <FreeTools />
-      </section>
-      <section aria-labelledby="FAQ  px-2">
-        <FAQ />
       </section>
     </main>
   );
